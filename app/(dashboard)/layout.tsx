f7aaabc376d6c39767d8a0f724817ddd12f7d9fb
tmalik258 <talhamalik25.tm@gmail.com>
2024-10-09 17:19:50 +0500

    added clerk, created sidebar

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,11 @@
+import SideBar from "./_components/sidebar";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+	return <div className="h-full">
+		<div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+			<SideBar />
+		</div>
+		{children}</div>;
+};
+
+export default DashboardLayout;

cff6ec902dfebdd775fae8861b178712c6bfea9a
tmalik258 <talhamalik25.tm@gmail.com>
2024-10-14 02:09:27 +0500

    fixed input.tsx interface issue

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,8 +2,10 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+	// Add any additional properties here
+	customProp?: string;
+  }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {

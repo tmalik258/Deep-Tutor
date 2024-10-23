a4635f6dabce5f66b7fda1a8931c064f861c56a7
tmalik258 <talhamalik25.tm@gmail.com>
2024-10-23 17:32:13 +0500

    Added category-form, and price-form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,12 +1,14 @@
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
-import { LayoutDashboard } from "lucide-react";
+import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
 import { redirect } from "next/navigation";
 import toast from "react-hot-toast";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
+import { CategoryForm } from "./_components/category-form";
+import { PriceForm } from "./_components/price-form";
 
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
@@ -22,6 +24,12 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 			userId,
 		},
 	});
+
+	const categories = await db.category.findMany({
+		orderBy: {
+			name: "asc"
+		}
+	})
 
 	if (!course) {
 		toast.error("This course doesn't exist.");
@@ -66,6 +74,34 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 						initialData={course}
 						courseId={course.id}
 					/>
+					<CategoryForm
+						initialData={course}
+						courseId={course.id}
+						options={categories.map(category => ({
+							label: category.name,
+							value: category.id
+						}))}
+					/>
+				</div>
+				<div className="space-y-6">
+					<div>
+						<div className="flex items-center gap-x-2">
+							<IconBadge icon={ListChecks} />
+							<h2 className="text-xl">
+								Course Chapters
+							</h2>
+						</div>
+						<div>TODO: Chapters</div>
+					</div>
+					<div>
+						<div className="flex items-center gap-x-2">
+							<IconBadge icon={CircleDollarSign} />
+							<h2 className="text-xl">
+								Sell your course
+							</h2>
+						</div>
+						<PriceForm initialData={course} courseId={course.id} />
+					</div>
 				</div>
 			</div>
 		</div>

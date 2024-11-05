e32c20b3cf82416ce0ebe3990122a0abcb149d26
samadsheikh99 <70121118@student.uol.edu.pk>
2024-11-05 19:50:43 +0500

    add course progress

---
+++ components/course-progres.tsx
@@ -0,0 +1,29 @@
+import {Progress} from "@/components/ui/progress";
+interface CourseProgressProps{
+value: number;
+variant?: "default" | "sm";
+};
+
+const colorByVariant={
+    default:"text-sky-700",
+    success: "text-emerald-700"
+}
+
+const sizeByVariant={
+    default:"text-sm",
+    sm: "text-xs"
+}
+export const CourseProgress =({
+    value,
+    variant,
+    size,
+}:CourseProgressProps) => {
+    return (
+        <div>
+            <Progress
+            className="h-2"
+            value={value}
+            />
+        </div>
+    )
+}

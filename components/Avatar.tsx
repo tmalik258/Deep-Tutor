69c94635b9a239e9e021ce918201042762d5d4ac
samadsheikh99 <70121118@student.uol.edu.pk>
2024-12-10 02:50:12 +0500

    add avatar

---
+++ components/Avatar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useGLTF } from '@react-three/drei';
+
+interface AvatarProps {
+  avatarUrl: string; // Explicitly defining the type of avatarUrl as string
+}
+
+const Avatar = ({ avatarUrl }: AvatarProps) => {
+  const { scene } = useGLTF(avatarUrl); // useGLTF provides the correct types internally
+  
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[10, 10, 5]} intensity={1} />
+      <primitive object={scene} scale={1.5} />
+      <OrbitControls />
+    </Canvas>
+  );
+};
+
+export default Avatar;
